eeb2dda8f5d332616284afdd95fae50ee9e7a24b
mcallyca-oss <mcallyca@gmail.com>
2026-08-23 23:19:55 +0700

    Add client registration

---
+++ src/main/java/com/vicky/bibliocraft/client/BiblioClient.java
@@ -0,0 +1,24 @@
+package com.vicky.bibliocraft.client;
+
+import com.vicky.bibliocraft.BiblioCraftLegacy;
+import com.vicky.bibliocraft.registry.BiblioMenus;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterMenuScreensEvent;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import com.vicky.bibliocraft.registry.BiblioBlockEntities;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid=BiblioCraftLegacy.MODID,bus=Mod.EventBusSubscriber.Bus.MOD,value=Dist.CLIENT)
+public final class BiblioClient {
+    @SubscribeEvent
+    public static void screens(RegisterMenuScreensEvent event){
+        event.register(BiblioMenus.BIBLIO_CONTAINER.get(), BiblioContainerScreen::new);
+    }
+
+    @SubscribeEvent
+    public static void renderers(EntityRenderersEvent.RegisterRenderers event){
+        event.registerBlockEntityRenderer(BiblioBlockEntities.BIBLIO_CONTAINER.get(), BiblioBlockEntityRenderer::new);
+    }
+}
